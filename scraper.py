32e03866f3be1e93f4baa8ada61696ebbe20e1fc
Manf <63849808+mnfro@users.noreply.github.com>
2024-08-08 12:55:12 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,8 +4,11 @@ import requests
 import os
 import time
 
-API_URL = os.environ['API_URL']
-HOST = os.environ['HOST']
+HOST = "www.dovevivo.it"
+
+ROOM_LIST = ["https://www.dovevivo.it/page-data/it/affitto-stanza-singola-milano-piazza-maria-adelaide-di-savoia/mi-0074-03-a/page-data.json",
+             "https://www.dovevivo.it/page-data/it/affitto-stanza-singola-milano-piazza-maria-adelaide-di-savoia/mi-0074-02-a/page-data.json",
+             "https://www.dovevivo.it/page-data/it/affitto-stanza-singola-milano-via-della-moscova/mi-1347-02-a/page-data.json"]
 
 HEADERS = {"Accept":"*/*",
            "Accept-Encoding":"gzip, deflate, br",
@@ -20,9 +23,9 @@ TOKEN = os.environ['TOKEN']
 CHAT_ID = os.environ['CHAT_ID']
 TL_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
-async def scrape():
+async def scrape(url):
    async with aiohttp.ClientSession() as session:
-      response = await session.get(url=API_URL, headers=HEADERS)
+      response = await session.get(url=url, headers=HEADERS)
       json_output = await response.json()
       await lookForIn(json_output)
 
@@ -32,15 +35,16 @@ async def lookForIn(json):
    room_availability = roomInfo['availability']
    #
    roomName = roomInfo['name']
+   path = json['path']
+   room_url = f'https://www.dovevivo.it/{path}'
    discount_until = room_commercial_conditions['discountUntil'][:-15]
    min_stay_date = room_commercial_conditions['minStayDate'][:-15]
    discounted_price = room_commercial_conditions['discountedPrice']
+   price = room_commercial_conditions['price']
    available_from = room_availability['availableFrom'][:-15]
-   #
-   if available_from == "2024-08-01":
-      telegram_message = f"The room is still available. Price at {discounted_price}€ until {discount_until}\n---\nMinimum staying date: {min_stay_date}"
-   else:
-      telegram_message = f"The room has been booked :'("
+
+   telegram_message = f'{roomName}\n{discounted_price}€ ({price}€) until {discount_until}\nAvailable from: {available_from}\nMin. staying: {min_stay_date}\n{room_url}'
+
    print(telegram_message)
    sendMessageToBot(telegram_message)
 
@@ -56,8 +60,9 @@ def sendMessageToBot(message):
 async def main():
    start_time = time.time()
    tasks = []
-   a_task = asyncio.create_task(scrape())
-   tasks.append(a_task)
+   for url in ROOM_LIST:
+      new_task = asyncio.create_task(scrape(url))
+      tasks.append(new_task)
    await asyncio.gather(*tasks)
    time_diff = time.time() - start_time
    print(f'Global time: %.3f seconds.' % time_diff)
